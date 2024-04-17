be7e1bf6a91dac4c7f02c8bbbdd7145373bf4d3b
Hans How <hanshow00@gmail.com>
2024-04-17 16:10:40 -0500

    complete looping code

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// Function to generate thank you messages for each name provided
+function writeCards(namesArray, eventName) {
+    const messagesArray = [];
+
+    for (let i = 0; i < namesArray.length; i++) {
+        const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+        messagesArray.push(message);
+    }
+
+    return messagesArray;
+}
+
+// Function to count down from a given number to zero
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
